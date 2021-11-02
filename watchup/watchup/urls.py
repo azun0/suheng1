e2c2fcf1355f1121eecc0af8c66db0600d3dc044
psuho <bagsuho45@gmail.com>
2021-11-02 11:53:23 +0900

    /
    /

--- watchup/watchup/urls.py
+++ watchup/watchup/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.views.generic import RedirectView
 
+# http://localhost/
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', include('home.urls')),
+    path('', RedirectView.as_view(url="/home/", permanent=True)),
+    path('user/', include('user.urls')),
+    path('board/', include('board.urls')),
 ]
