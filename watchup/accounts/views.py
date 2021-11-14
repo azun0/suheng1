d2b76480d667f9842fe1f46b85590cdc8a0c5e61
jh17ha <93509189+jh17ha@users.noreply.github.com>
2021-11-14 18:07:42 +0900

    /

--- watchup/accounts/views.py
+++ watchup/accounts/views.py
@@ -1,19 +1,18 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from .forms import SignUpForm
 
 def signup(request) :
     if request.method == 'POST':
-        username=request.POST.get('username')
-        password = request.POST.get('password')
-        password2 = request.POST.get('password2')
-        print(username, password, password2)
+        signup_form = SignUpForm(request.POST)
+
+        if signup_form.is_valid():
+            user_instance = signup_form.save(commit=False)
+            user_instance.set_password(signup_form.cleaned_data['password'])
+            user_instance.save()
+            return render(request, 'accounts/signup_complete.html', {'username':user_instance.username})
         
-        user=User()
-        user.username = username
-        user.set_password(password)
-        user.save()
-        
-        return render(request, 'accounts/signup_complete.html')
     else:
-        context_values = {'form' : 'this is form'}
-        return render(request, 'accounts/signup.html', context_values)
+        signup_form = SignUpForm()
+
+    return render(request, 'accounts/signup.html', {'form':signup_form.as_p})
