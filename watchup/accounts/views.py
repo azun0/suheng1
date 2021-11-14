7cb5adc87d5c9cd1dc196699d9479d2dac1ebeaf
jh17ha <93509189+jh17ha@users.noreply.github.com>
2021-11-14 17:09:06 +0900

    /

--- watchup/accounts/views.py
+++ watchup/accounts/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
-
-# Create your views here.
+def signup(request) :
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        print(username, password, password2)
+        
+        user=User()
+        user.username = username
+        user.password = password
+        user.save()
+        return render(request, 'accounts/signup_complete.html')
+    else:
+        context_values = {'form' : 'this is form'}
+        return render(request, 'accounts/signup.html', context_values)
