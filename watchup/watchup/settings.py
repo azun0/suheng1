c93bb482804e7dd3d296251ec8bf44ca42d71d35
azun0 <92145731+azun0@users.noreply.github.com>
2021-11-14 16:39:33 +0900

    .

--- watchup/watchup/settings.py
+++ watchup/watchup/settings.py
@@ -18,7 +18,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SITE_ID = 1
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
