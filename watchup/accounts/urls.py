e24fc0b17f683d9f1c9332a49cd0e5110def2307
azun0 <92145731+azun0@users.noreply.github.com>
2021-11-14 17:09:10 +0900

    .

--- watchup/accounts/urls.py
+++ watchup/accounts/urls.py
@@ -4,6 +4,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 app_name = "accounts"
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name = 'templates/accounts/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name = 'templates/accounts/logout.html'), name='logout'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
