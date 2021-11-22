0594a13ebeaaebab5bf2c36593b1a3113fc06d88
scentB <chic464748@naver.com>
2021-11-22 15:53:02 +0900

    /

--- watchup/watchup/settings.py
+++ watchup/watchup/settings.py
@@ -16,6 +16,8 @@ from django.contrib.auth import get_user_model as user_model
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 SITE_ID = 1
